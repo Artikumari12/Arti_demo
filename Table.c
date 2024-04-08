3956e6465ba69be4e5a1ebed4bf6048fa8ab2aee
Artikumari12 <152809579+Artikumari12@users.noreply.github.com>
2024-04-08 23:20:30 +0530

    Add files via upload

---
+++ Table.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+int num,table=1;
+printf("Enter the number");
+scanf("%d",&num);
+for(int i=1;i<=10;i++)
+{
+    table=num*i;
+    printf("%d\n",table);
+}
+    return 0;
+
+}
